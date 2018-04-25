95c867c2914fd9c8c9164f20c6c09b1b98ca4a5f
zzq <Keen_Team@163.com>
2018-04-25 09:52:49 +0800

    CZHDatePickerView

---
+++ CZHDatePickerView/ViewController.m
@@ -0,0 +1,49 @@
+//
+//  ViewController.m
+//  CZHDatePickerView
+//
+//  Created by 程召华 on 2017/11/20.
+//  Copyright © 2017年 程召华. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "DatePickerHeader.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *haveDateLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *noDateLabel;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+}
+- (IBAction)haveDateClick:(id)sender {
+    CZHWeakSelf(self);
+    [CZHDatePickerView sharePickerViewWithCurrentDate:self.haveDateLabel.text DateBlock:^(NSString *dateString) {
+        CZHStrongSelf(self);
+        self.haveDateLabel.text = dateString;
+    }];
+    
+}
+
+- (IBAction)noDateClick:(id)sender {
+    CZHWeakSelf(self);
+    [CZHDatePickerView sharePickerViewWithCurrentDate:@"" DateBlock:^(NSString *dateString) {
+        CZHStrongSelf(self);
+        self.noDateLabel.text = dateString;
+    }];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
